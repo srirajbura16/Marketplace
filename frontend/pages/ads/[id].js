5146e81628a961740c06bd75c5a26309a2c17cf4
Sriraj Bura <sriraj.bura16@gmail.com>
2021-12-10 08:24:26 -0500

    added dynamic pages and component

--- frontend/pages/ads/[id].js
+++ frontend/pages/ads/[id].js
@@ -1,7 +1,24 @@
 // list ad deatils, along with user info.
-import React from 'react';
 import Ad from '../../components/Ads/Ad';
 
-export default function AdDetails({ ad, user }) {
-  return <div></div>;
+export default function AdDetails({ data: ad }) {
+  console.log(ad);
+  return <div>ad</div>;
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  console.log(id);
+  const res = await fetch(`http://localhost:5000/api/ads/${id}`);
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+  };
 }
--- frontend/pages/users/[id].js
+++ frontend/pages/users/[id].js
@@ -1,1 +1,22 @@
 //list user info, listings
+import React from 'react';
+
+export default function User({ data: user }) {
+  return <div>user</div>;
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const res = await fetch(`http://localhost:5000/api/users/${id}`);
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+  };
+}
