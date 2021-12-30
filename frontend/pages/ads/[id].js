92ca6301cfa49d8a8212002250ed1f51f1670e95
Sriraj Bura <sriraj.bura16@gmail.com>
2021-12-30 17:34:49 -0500

    added ad page

--- frontend/pages/ads/[id].js
+++ frontend/pages/ads/[id].js
@@ -1,9 +1,21 @@
-// list ad deatils, along with user info.
-import Ad from '../../components/Ads/Ad';
+import styles from '../../styles/AdDetails.module.css';
+// import Ad from ''
 
 export default function AdDetails({ data: ad }) {
-  console.log(ad);
-  return <div>ad</div>;
+  console.log(ad, 'FROM COMPONENT');
+  const { title, description, price, condition, image_url } = ad;
+  return (
+    <div className={styles.ad}>
+      <div className={styles.ad_image}>
+        <img src={image_url} alt="ad image" />
+      </div>
+      <h1>{title}</h1>
+      <h1>{price}</h1>
+      <hr />
+      <div className={styles.description}>{description}</div>
+      <div className={styles.condition}>{condition}</div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
