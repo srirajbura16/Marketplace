bd462afe455baac118ff0c34d6a5eaacf4142a2f
Sriraj Bura <sriraj.bura16@gmail.com>
2022-02-04 17:18:01 -0500

    shifted data fetching ads from home page to component, to ensure home page is always rendered

--- frontend/components/Ads/Ads.js
+++ frontend/components/Ads/Ads.js
@@ -1,32 +1,50 @@
 import styles from '../../styles/Ads.module.css';
 import Link from 'next/link';
+import API_URL from '../../lib/API_URL';
+import { useState, useEffect } from 'react';
 
-export default function Ads({ ads }) {
+export default function Ads({ data }) {
+  const [message, setMessage] = useState('Loading...');
+  const [ads, setAds] = useState();
+  useEffect(async () => {
+    const res = await fetch(`${API_URL}/api/ads`);
+    const data = await res.json();
+
+    if (!data) {
+      setMessage('failed to fetch data');
+    } else {
+      setAds(data);
+    }
+  }, []);
   return (
     <div className={styles.ads}>
-      {ads.map((ad) => {
-        const { title, price, condition, image_url, _id } = ad;
-        return (
-          <div className={styles.ad} key={_id}>
-            <div className={styles.ad_image}>
-              <img
-                src={image_url}
-                className={styles.img}
-                alt="ad picture"
-                width={400}
-                height={300}
-              />
+      {ads ? (
+        ads.map((ad) => {
+          const { title, price, condition, image_url, _id } = ad;
+          return (
+            <div className={styles.ad} key={_id}>
+              <div className={styles.ad_image}>
+                <img
+                  src={image_url}
+                  className={styles.img}
+                  alt="ad picture"
+                  width={400}
+                  height={300}
+                />
+              </div>
+              <div className="price">$ {price}</div>
+              <Link href={`/posts/${_id}`}>
+                <a>
+                  <div className={styles.title}>{title}</div>
+                </a>
+              </Link>
+              <div className={styles.condition}>{condition}</div>
             </div>
-            <div className="price">$ {price}</div>
-            <Link href={`/posts/${_id}`}>
-              <a>
-                <div className={styles.title}>{title}</div>
-              </a>
-            </Link>
-            <div className={styles.condition}>{condition}</div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <h3>{message}</h3>
+      )}
     </div>
   );
 }
